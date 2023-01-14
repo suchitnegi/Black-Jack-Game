23792060af1c7522b5b8e13cc60c5fd0361a2070
Suchit Negi <suchitnegi99@gmail.com>
2023-01-15 02:39:50 +0530

    learning javascript
    learned different data types, functions, objects in JS and different math function which plays an important role in game development.

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+
+let cards = []
+let sum = 0
+
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.getElementById("card-el")
 
-function newCard()
-{
-    console.log("Drawing a New card from the deck")
+
+let player = {
+
+    name: "Suchit",
+    chip: 130
 }
-function startGame(){
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chip
+
+
+
+function getRandomCard()
+{
+    let randomNumber = Math.floor(Math.random()*13) + 1
+    
+    if(randomNumber>10)
+    {return 10}
+    else if(randomNumber ==1)
+    {
+    return 11
+    }else{
+        return randomNumber
+    }
+}
+
+
+function newCard()
+{   
+    if(isAlive === true && hasBlackJack === false)
+    
+    {
+    let card = getRandomCard()
+    cards.push(card) 
+    console.log("Drawing a New card from the deck")
+    sum+=card
+    renderGame()
+    }
+} 
+function startGame()
+{
+     isAlive = true
+     let firstCard = getRandomCard()
+     let secondCard = getRandomCard()
+     cards = [firstCard,secondCard]
+     sum = cards[0] + cards[1]
+     hasBlackJack === false
+    
+    
+    renderGame()
+}
+function renderGame(){
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -25,7 +72,12 @@ function startGame(){
     }
     sumEl.textContent = "Sum: " + sum 
     messageEl.textContent = message 
-    cardEl.textContent = "Cards: "+firstCard +" "+ secondCard
+    cardEl.textContent = "Cards: "
+    for(let i = 0 ; i < cards.length ; i++)
+    {
+        cardEl.textContent += cards[i] + " "
+    }
+    
     
 
 }
